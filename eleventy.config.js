55446775b4b1b514a857f51c23d2667b99c08dc5
Steve Cherry <stevenccherry@gmail.com>
2022-03-15 09:40:24 -0400

    Remove navigation

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,4 @@
-const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(eleventyNavigationPlugin);
-
   eleventyConfig.addPassthroughCopy('./public');
   eleventyConfig.addPassthroughCopy('./src/sw.js');
 
--- src/index.md
+++ src/index.md
@@ -1,9 +1,6 @@
 ---
 layout: default
 title: Home
-eleventyNavigation:
-  key: Home
-  order: -1
 ---
 
 Hello world!
